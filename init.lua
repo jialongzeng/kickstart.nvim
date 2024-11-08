4c7c125e2e26bc6da4acc900ccc1a263fbae7e2b
jialongzeng <colenz@qq.com>
2024-11-09 00:03:50 +0800

    trivial fix

--- init.lua
+++ init.lua
@@ -1011,7 +1011,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
       task = '📌',
       lazy = '💤 ',
     },
-  })
+  }
+})
 
   -- The line beneath this is called `modeline`. See `:help modeline`
   -- vim: ts=2 sts=2 sw=2 et
